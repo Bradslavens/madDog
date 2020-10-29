3422c798134c47069558871af3e353db93735f4a
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-10-29 10:42:22 -0700

    add force to shoot player across screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    Vector3 initialPosition;
+    Rigidbody2D playerRB;
+
+    [SerializeField] float velocity;
+
+    private void Awake() {
+        initialPosition = transform.position;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,8 +30,17 @@ public class PlayerController : MonoBehaviour
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         newPosition.z = 0;
-        
+
         transform.position= newPosition;
     
+    }
+
+    private void OnMouseUp() {
+        
+        Vector2 direction = initialPosition - transform.position;
+
+        playerRB.AddForce(direction * velocity);
+        
+        playerRB.gravityScale = 1;
     }
 }
